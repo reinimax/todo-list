8ce3c431dc11f8d4d0814473a6b3cce72d391339
reini <reinhold.schachner@hotmail.com>
2020-10-11 13:52:12 +0200

    Add listeners to buttons to set boxes visible/invisible

--- src/displaycontroller.js
+++ src/displaycontroller.js
@@ -2,7 +2,25 @@ const displayController = (() => {
     //cacheDOM
     const newProjectBtn = document.querySelector("#new-project");
     const newToDoBtn = document.querySelector("#new-todo");
+    const createProjectBtn = document.querySelector("#create-project");
+    const createToDoBtn = document.querySelector("#create-todo");
     const navBarLinks = document.querySelector("#navbar-links");
+    const newProjectForm = document.querySelector("#new-project-form");
+    const newToDoForm = document.querySelector("#new-todo-form");
+    
+    //add listeners
+    newProjectBtn.addEventListener("click", () => setVisible(newProjectForm) );
+    newToDoBtn.addEventListener("click", () => setVisible(newToDoForm) );
+    createProjectBtn.addEventListener("click", () => setInvisible(newProjectForm) );
+    createToDoBtn.addEventListener("click", () => setInvisible(newToDoForm) );
+    
+    function setVisible(popupBox) {
+        popupBox.classList.add("visible");
+    }
+
+    function setInvisible(popupBox) {
+        popupBox.classList.remove("visible");
+    }
     
     function renderNavBar(arrayToRender) {
         clearDisplayElement(navBarLinks);
