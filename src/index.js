18657c39c92c1129510ff33bdc14782901834dc8
reini <reinhold.schachner@hotmail.com>
2020-10-11 09:43:12 +0200

    Add function for creating todos in a specified project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import ToDo from "./todo.js";
+import Project from "./project.js";
 
-console.log(new ToDo("Test", "This is a test", "anymtime", "no priority"));
+let defaultProject = new Project("default");
+
+function createToDo(name, description, dueDate, priority, project) {
+    project.addToDo(new ToDo(name, description, dueDate, priority));
+}
+
+createToDo("Test", "This is a test", "anymtime", "no priority", defaultProject);
+
+console.table(defaultProject.toDoList);
