953b6ef09cb6e37eee6dabb940e11f6dfdc63da2
reini <reinhold.schachner@hotmail.com>
2020-10-11 10:37:42 +0200

    Add deleteProject function

--- src/index.js
+++ src/index.js
@@ -15,8 +15,29 @@ function findProject(project) {
     return board.find( (obj) => obj.name === project);
 }
 
+function deleteProject(project) {
+    const indexToDelete = board.indexOf(findProject(project));
+    board.splice(indexToDelete, 1);
+}
+
 //for testing
 createProject("default");
 createToDo("Test", "This is a test", "anymtime", "no priority", "default");
 
 console.table(findProject("default").toDoList);
+
+createToDo("Test2", "This is a test", "anymtime", "no priority", "default");
+
+console.table(findProject("default").toDoList);
+
+findProject("default").deleteToDo("Test2");
+
+console.table(findProject("default").toDoList);
+
+createProject("default2");
+
+console.table(board);
+
+deleteProject("default2");
+
+console.table(board);
