f6ba1364eb76adf53d607cffa51bc188b4cc4203
reini <reinhold.schachner@hotmail.com>
2020-10-14 11:16:58 +0200

    Wrap coordinator in an iife

--- src/index.js
+++ src/index.js
@@ -4,60 +4,64 @@ import "./style.css";
 import boardController from "./boardcontroller";
 import displayController from "./displaycontroller";
 
-// this will become the coordinator for the whole thing
-const createProjectBtn = document.querySelector("#create-project");
-const createToDoBtn = document.querySelector("#create-todo");
+(function coordinator() {
+  // functions
+  function createProject(name) {
+    boardController.createProject(name);
+    displayController.renderNavBar(boardController.getBoard());
+  }
 
-const newProjectName = document.querySelector("#project-name");
+  function createToDo(name, description, dueDate, priority, project) {
+    // not yet implemented
+  }
 
-createProjectBtn.addEventListener("click", () =>
-  createProject(newProjectName.value)
-);
-createToDoBtn.addEventListener("click", () =>
-  createToDo(name, description, dueDate, priority, project)
-);
+  // cacheDom
+  const createProjectBtn = document.querySelector("#create-project");
+  const createToDoBtn = document.querySelector("#create-todo");
 
-function createProject(name) {
-  boardController.createProject(name);
+  const newProjectName = document.querySelector("#project-name");
+
+  // add listeners
+  createProjectBtn.addEventListener("click", () =>
+    createProject(newProjectName.value)
+  );
+  createToDoBtn.addEventListener("click", () =>
+    createToDo(name, description, dueDate, priority, project)
+  );
+
+  // for testing
+  boardController.createProject("default");
+  boardController.createToDo(
+    "Test",
+    "This is a test",
+    "anymtime",
+    "no priority",
+    "default"
+  );
+
+  console.table(boardController.findProject("default").toDoList);
+
+  boardController.createToDo(
+    "Test2",
+    "This is a test",
+    "anymtime",
+    "no priority",
+    "default"
+  );
+
+  console.table(boardController.findProject("default").toDoList);
+
+  boardController.findProject("default").deleteToDo("Test2");
+
+  console.table(boardController.findProject("default").toDoList);
+
+  boardController.createProject("default2");
+
+  console.table(boardController.getBoard());
+
+  boardController.deleteProject("default2");
+
+  console.table(boardController.getBoard());
+
   displayController.renderNavBar(boardController.getBoard());
-}
-
-function createToDo(name, description, dueDate, priority, project) {
-  // not yet implemented
-}
-
-// for testing
-boardController.createProject("default");
-boardController.createToDo(
-  "Test",
-  "This is a test",
-  "anymtime",
-  "no priority",
-  "default"
-);
-
-console.table(boardController.findProject("default").toDoList);
-
-boardController.createToDo(
-  "Test2",
-  "This is a test",
-  "anymtime",
-  "no priority",
-  "default"
-);
-
-console.table(boardController.findProject("default").toDoList);
-
-boardController.findProject("default").deleteToDo("Test2");
-
-console.table(boardController.findProject("default").toDoList);
-
-boardController.createProject("default2");
-
-console.table(boardController.getBoard());
-
-boardController.deleteProject("default2");
-
-console.table(boardController.getBoard());
-
-displayController.renderNavBar(boardController.getBoard());
+})();
